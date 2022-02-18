0e6def9f66346bd5009938b64d3eab4d5b972296
Rahul <dnrp1999@gmail.com>
2022-02-18 16:32:58 +0530

    minor

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import JoinRoom from "./components/JoinRoom";
 import Chat from "./components/Chat";
 import { useState } from "react";
 import io from "socket.io-client";
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chatapphere.herokuapp.com/");
 
 function App() {
   const [Name, setName] = useState("");
